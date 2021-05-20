649dcd3ee1797023b0dfddde23f396d7ac4092f7
dario <tacachojose@gmail.com>
2021-05-20 18:50:12 -0300

    probando recorrer el objeto

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 function mostrarMeDatos() {
-  fetch("datos.json")
-    .then((respuesta) => respuesta.json())
-    .then((respuesta) => console.log(respuesta));
+  console.log(
+    fetch("datos.json")
+      .then((res) => res.json())
+      .then((res) => imprimirEnConsol(res))
+  );
 }
+function imprimirEnConsol(datos) {
+  let obj = JSON.parse(JSON.stringify(datos));
+  console.log(obj);
+  for (const prop in obj) {
+    if (Object.keys(prop) === "site_id") {
+      console.log("lo tenemos");
+    }
+  }
 
+  for (let index = 0; index < Object.keys(obj); index++) {
+    console.log(index);
+    console.log("asd");
+  }
+  console.log("probando");
+}
 mostrarMeDatos();
