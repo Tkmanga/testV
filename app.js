b81f3c0b655cf4d5db36a65081721c73750970d2
dario <tacachojose@gmail.com>
2021-05-21 12:58:18 -0300

    agregando 2da llamada al API en base al primero, cambiando por asyn ?...

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let categorias = {};
 let resultado = {};
+let nombreCategoria = [];
 const fetch = require("node-fetch");
 const fs = require("fs");
 const archivo = "texto.txt";
@@ -17,14 +18,20 @@ function buscar() {
 }
 
 function init() {
-  fetch("https://api.mercadolibre.com/categories/MLA1720")
+  fetch("https://api.mercadolibre.com/sites/MLA/search?seller_id=179571326", {
+    Headers: {
+      Authorization:
+        "Bearer APP_USR-1850140775408492-052114-6b8ecc3a7ad457469737e9f2d1f0c224-101318101",
+    },
+  })
     .then((res) => res.json())
-    .then((res) => imprimirEnConsol(res));
+    .then((res) => {
+      resultado = res;
+      imprimirEnConsola(resultado);
+    });
 }
-function imprimirEnConsol(datos) {
+function imprimirEnConsola(datos) {
   let obj = JSON.parse(JSON.stringify(datos));
-  resultado = obj["results"];
-  categorias = obj["available_filters"][0]["values"];
 
   for (var key1 in obj) {
     if (key1 == "results") {
@@ -34,15 +41,35 @@ function imprimirEnConsol(datos) {
 }
 
 function recorrerObjeto(obj) {
-  let nombreCategoria = consultar(obj[0]["category_id"]);
-
   obj.forEach((element) => {
-    console.log(
-      `${element["id"]} ${element["title"]} ${element["category_id"]}`
-    );
+    traerNombreCategoria(element["category_id"]).then((r) => console.log(r));
   });
+  crearString(obj);
 }
 
-function consultar() {}
+function traerNombreCategoria(itemName) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        fetch(`https://api.mercadolibre.com/categories/${itemName}`)
+          .then((res) => res.json())
+          .then((res) => {
+            console.log(res.name);
+            nombreCategoria.push(res.name);
+          })
+      );
+    }, 3000);
+  });
+}
+function crearString(obj) {
+  console.log(nombreCategoria);
+  obj.forEach((element, index) => {});
+  /*
+  console.log(
+    `Id: {${element["id"]}} Titulo: {${element["title"]}} Id Categoria: {${element["category_id"]}} Nombre de la categoria ${nombreCategoria}`
+  );
+  */
+}
+function imprimirCadena(a) {}
 
 init();
